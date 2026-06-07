c0c1e7786a934123cf66d44d29b8c3f9209cac0e
Keerthana R <keerthana241495@gmail.com>
2026-06-07 15:00:40 +0530

    set aria to false for eslint for deployment

--- frontend/src/components/UI/searchbar.js
+++ frontend/src/components/UI/searchbar.js
@@ -42,13 +42,13 @@ const SearchBar = () => {
       />
       <span className="ava-search-icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
       </span>
       {results.length > 0 && (
         <ul className="ava-search-results" role="listbox">
           {results.map((r) => (
-            <li key={r.slno} role="option" onClick={() => handleSelect(r.path)}>
+            <li key={r.slno} role="option" aria-selected="false" onClick={() => handleSelect(r.path)}>
               {r.name || r.catID__name}
             </li>
           ))}
